0e9466b34a2e6808b5a33c082a6c61ca90f9f3b3
Robin Schroer <sulami@peerwire.org>
2019-02-05 10:58:45 +0100

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject james-cli "0.1.0"
+(defproject james-cli "0.1.1-SNAPSHOT"
   :description "James Launcher - CLI version"
   :url "http://github.com/sulami/james-cli"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
